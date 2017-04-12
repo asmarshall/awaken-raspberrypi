63af8a117a2b45502790e0edd8d0cbd6054f5ec0
radiohegel <roger.e.clayton@gmail.com>
2017-04-12 08:14:35 -0700

    awaken_shake sending html request and receiving accessible JSON data back

--- awaken_shake.py
+++ awaken_shake.py
@@ -1,7 +1,16 @@
-import webbrowser
-import json
-import requests
-from pubnub import Pubnub
+# import webbrowser
+# import json
+# import requests
+# from pubnub import Pubnub
+#
+# r = requests.get('https://localhost:3000/devices/1')
+# r.json()
 
-r = requests.get('https://localhost:3000/devices/1')
-r.json()
+import json, requests
+
+url = 'http://localhost:3000/devices/1'
+
+resp = requests.get(url=url)
+data = json.loads(resp.text)
+
+print data["message"]
